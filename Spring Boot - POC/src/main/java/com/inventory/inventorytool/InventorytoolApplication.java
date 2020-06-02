fb0fd3c6cec97c5685901072a1463a8c9969b49a
sharathn29 <nsharathn@gmail.com>
2020-06-02 00:52:00 -0400

    first commit

---
+++ Spring Boot - POC/src/main/java/com/inventory/inventorytool/InventorytoolApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.inventorytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorytoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorytoolApplication.class, args);
+    }
+
+}
---
+++ Spring Boot - POC/src/test/java/com/inventory/inventorytool/InventorytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.inventorytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorytoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
